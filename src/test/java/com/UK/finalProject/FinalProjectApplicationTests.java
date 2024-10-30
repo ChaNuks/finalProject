cbc5ab19ec98bf41ffce2d9720338c03d6d0412f
macbook <cksdnrtls33@gmail.com>
2024-10-30 20:49:54 +0900

    INIT: 프로젝트 생성
    - Java 17
    - SpringBoot 3.3.5
    - MySQL
    - JDBC

---
+++ src/test/java/com/UK/finalProject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UK.finalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
